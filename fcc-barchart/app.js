aa82add17bbb3dc458ef6da2f4a3bfe5c5024140
Kumar Gaurav <kmrgaurav33@gmail.com>
2020-09-23 12:09:22 +0530

    axes added to graph

--- fcc-barchart/app.js
+++ fcc-barchart/app.js
@@ -1,11 +1,18 @@
 import data from './data.js'
 
-const { select, scaleLinear, max, scaleBand } = d3
+const {
+  axisBottom,
+  axisLeft,
+  max,
+  scaleBand,
+  scaleLinear,
+  select
+} = d3
 
 const margin = {
   bottom: 100,
   left: 100,
-  right: 50,
+  right: 40,
   top: 50,
 }
 
@@ -13,6 +20,8 @@ const svg = select('svg')
 
 const width = +svg.attr('width')
 const height = +svg.attr('height')
+const innerWidth = width - margin.left - margin.right
+const innerHeight = height -margin.top - margin.bottom
 
 const render = (data) => {
   // data Accessors
@@ -22,21 +31,32 @@ const render = (data) => {
   // scales
   const xScale = scaleLinear()
     .domain([0, max(data, xValue)])
-    .range([0, width - margin.left - margin.right])
+    .range([0, innerWidth])
   const yScale = scaleBand()
     .domain(data.map(yValue))
-    .range([0, height])
-
+    .range([0, innerHeight])
+    .padding(0.16)
+  
   // d3 magic
-  svg.selectAll('rect')
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+  
+  // axes
+  g.append('g').call(axisLeft(yScale))
+    .selectAll('text')
+    .attr('class', 'text-y')
+  g.append('g').call(axisBottom(xScale))
+    .attr('transform', `translate(0, ${innerHeight})`)
+
+  // rendering bars
+  g.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
+    .classed('bars', true)
     .attr('y', d => yScale(yValue(d)))
     .attr('width', d => xScale(xValue(d)))
     .attr('height', yScale.bandwidth())
 }
 
 render(data)
-
-console.log([width, height])
